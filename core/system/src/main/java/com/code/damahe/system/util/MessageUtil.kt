25e88704f63a1e1a1da81088f307ff8e811853ea
Damahe Code <damahecode@gmail.com>
2024-06-30 11:37:31 +0530

    relative time

--- core/system/src/main/java/com/code/damahe/system/util/MessageUtil.kt
+++ core/system/src/main/java/com/code/damahe/system/util/MessageUtil.kt
@@ -1,8 +1,21 @@
 package com.code.damahe.system.util
 
+import android.text.format.DateUtils
+import java.text.SimpleDateFormat
+import java.time.DayOfWeek
+import java.util.Calendar
+import java.util.Date
 
 
 object MessageUtil {
 
+    fun convertTimeMillis(millis: Long): String {
+        val simpleDateFormat = SimpleDateFormat.getDateTimeInstance()
+        val date = Date(millis)
 
+//        val day = simpleDateFormat.calendar.get(Calendar.DAY_OF_WEEK)
+        val relative = DateUtils.getRelativeTimeSpanString(date.time)
+
+        return relative.toString() // simpleDateFormat.format(date)
+    }
 }
